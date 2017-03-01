fcbe968aaf36e74f4ca5c1f9b7d5ea69ae08eff5
David <david>
2017-03-01 09:14:45 +0100

    Crear Boton (vacaciones) logistica-produccion David

--- Vista/Plantilla/cabecera.php
+++ Vista/Plantilla/cabecera.php
@@ -69,7 +69,7 @@
                 <?php if(!parent::isRoot()){
                     ?>
                     <li><a href="<?php echo parent::getUrlRaiz()?>/Vista/Horario/Horario.php">Horario Semanal</a></li>
-                    <li><a href="<?php echo parent::getUrlRaiz()?>/Vista/Calendario/Calendario.php">Vacaciones/Incidencias</a></li>
+                    <li><a href="<?php echo parent::getUrlRaiz()?>/Vista/Calendario/Calendario.php">Vacaciones/Incidencias</a></li> <!--Cambio David-->
                     <li><a href="<?php echo parent::getUrlRaiz()?>/Vista/Calendario/Calendario.php">Partes</a></li>
                 <?php
                 }

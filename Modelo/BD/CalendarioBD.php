399cd8c12f282f2f1406a090d5a5b5965f3d090b
aitortarazaga <baskonistaat@gmail.com>
2017-03-03 10:03:40 +0100

    Comprobar calendarios creados Aitor

--- Modelo/BD/CalendarioBD.php
+++ Modelo/BD/CalendarioBD.php
@@ -21,17 +21,24 @@ abstract class CalendarioBD extends GenericoBD   //Aitor
 
     public static function crearCalendario($calendario)      // IRUNE
     {
+        if(self::comprobarEstadoCalend($calendario)==true)
+        {
+            echo "Error, calendario ya creado";
+            return false;
+        }
 
-        $con = self::conectar();
+        else
+        {
+            $con = self::conectar();
 
-        $query = "INSERT INTO calendario VALUES ('".$calendario->getId()."', '".$calendario->getDesc()."', '".$calendario->getEstado()."')";
+            $query = "INSERT INTO calendario VALUES ('".$calendario->getId()."', '".$calendario->getDesc()."', '".$calendario->getEstado()."')";
 
-        $rs = mysqli_query($con, $query) or die("Error crearCalendario");
+            $rs = mysqli_query($con, $query) or die("Error crearCalendario");
 
-        self::desconectar($con);
+            self::desconectar($con);
 
-        return true;
-
+            return true;
+        }
     }
 
 
@@ -39,7 +46,21 @@ abstract class CalendarioBD extends GenericoBD   //Aitor
         $con=self::conectar();
         $query="UPDATE calendario set estado=2 WHERE id=".$id;
         $rs = mysqli_query($con, $query) or die("Error al cerrar Calendario");
+        self::desconectar($con);
         return true;
+    }
+
+
+    public static function comprobarEstadoCalend($calendario){  //Aitor
+        $con = self::conectar();
+
+        $query="SELECT * FROM calendario WHERE id=".$calendario->getId();
+        $rs = mysqli_query($con, $query) or die("Error al comprobar el estado del calendario");
+        if($rs===true){
+            return true;
+        }
+        else
+            return false;
     }
 }
 

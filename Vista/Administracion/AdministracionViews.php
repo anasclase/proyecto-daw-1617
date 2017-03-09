85d7ead9bebc6943a619de67f89209995449d17d
Pabs1921 <pabsgarcia96@gmail.com>
2017-03-09 11:57:28 +0100

    arreglo volver

--- Vista/Administracion/AdministracionViews.php
+++ Vista/Administracion/AdministracionViews.php
@@ -322,12 +322,7 @@ abstract class AdministracionViews extends \Vista\Plantilla\Views
                             <input class="form-control" type="text" name="nif" maxlength="9" required>
                         </div>
                     </div>
-                    <div class="form-group"><!--Ganeko-->
-                        <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
-                            <input class="btn btn-primary" type="submit" name="addEmpresa" value="Añadir">
-                            <input class="btn btn-warning" type="submit" name="volver" value="Volver">
-                        </div>
-                    </div>
+                    
                 </fieldset>
             </form><form class="form-horizontal" name="atras" method="post" action="<?php echo self::getUrlRaiz()?>/Controlador/Administracion/Router.php"<div class="form-group"><!--Ganeko-->
                         <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
@@ -638,12 +633,7 @@ abstract class AdministracionViews extends \Vista\Plantilla\Views
                                 <input class="form-control" type="text" name="marca" required>
                             </div>
                         </div>
-                        <div class="form-group"><!--Ganeko-->
-                            <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
-                                <input class="btn btn-primary" type="submit" value="Añadir" name="addVehiculo">
-                                <input class="btn btn-warning" type="submit" name="volver" value="Volver">
-                            </div>
-                        </div>
+                        
                     </fieldset>
                 </form><form class="form-horizontal" name="atras" method="post" action="<?php echo self::getUrlRaiz()?>/Controlador/Administracion/Router.php"<div class="form-group"><!--Ganeko-->
                         <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
@@ -760,12 +750,7 @@ abstract class AdministracionViews extends \Vista\Plantilla\Views
                             <input class="form-control" type="text" name="denominacion" required>
                         </div>
                     </div>
-                    <div class="form-group"><!--Ganeko-->
-                        <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
-                            <input class="btn btn-primary" type="submit" value="Añadir" name="addHorasConvenio">
-                            <input class="btn btn-warning" type="submit" name="volver" value="Volver">
-                        </div>
-                    </div>
+                    
                 </fieldset>
             </form><form class="form-horizontal" name="atras" method="post" action="<?php echo self::getUrlRaiz()?>/Controlador/Administracion/Router.php"<div class="form-group"><!--Ganeko-->
                         <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
@@ -976,12 +961,7 @@ abstract class AdministracionViews extends \Vista\Plantilla\Views
                             <input class="form-control" type="text" name="precio" required>
                         </div>
                     </div>
-                    <div class="form-group"><!--Ganeko-->
-                        <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
-                            <input class="btn btn-primary" type="submit" value="Añadir" name="addTipoFranja">
-                            <input class="btn btn-warning" type="submit" name="volver" value="Volver">
-                        </div>
-                    </div>
+                    
                 </fieldset>
             </form><form class="form-horizontal" name="atras" method="post" action="<?php echo self::getUrlRaiz()?>/Controlador/Administracion/Router.php"<div class="form-group"><!--Ganeko-->
                         <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
@@ -1178,12 +1158,7 @@ abstract class AdministracionViews extends \Vista\Plantilla\Views
                         </select>
                     </div>
                 </div>
-                <div class="form-group"><!--Ganeko-->
-                    <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
-                        <input class="btn btn-primary" type="submit" value="Añadir" name="addHorario">
-                        <input class="btn btn-warning" type="submit" name="volver" value="Volver">
-                    </div>
-                </div>
+                
 
             </form><form class="form-horizontal" name="atras" method="post" action="<?php echo self::getUrlRaiz()?>/Controlador/Administracion/Router.php"<div class="form-group"><!--Ganeko-->
                         <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
@@ -1309,12 +1284,7 @@ abstract class AdministracionViews extends \Vista\Plantilla\Views
                         </select>
                     </div>
                 </div>
-                <div class="form-group"><!-- Ganeko -->
-                    <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
-                        <input class="btn btn-primary" type="submit" value="Añadir" name="añadirHorarioTrabajador">
-                        <input class="btn btn-warning" type="submit" name="volver" value="Volver">
-                    </div>
-                </div>
+                
             </form><form class="form-horizontal" name="atras" method="post" action="<?php echo self::getUrlRaiz()?>/Controlador/Administracion/Router.php"<div class="form-group"><!--Ganeko-->
                         <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
                             <input class="btn btn-primary" type="submit" name="añadirHorarioTrabajador" value="Añadir">
